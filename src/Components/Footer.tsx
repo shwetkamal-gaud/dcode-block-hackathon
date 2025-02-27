4fc00887125ecde290202392d1aa317cabf8dded
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-02-27 21:48:19 +0530

    background and scroll bar changes

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-      <div className="flex bg-[#241E24] p-10 justify-between text-white">
+      <div className="flex bg-[#241E24] py-10 px-20 justify-between text-white">
           <p>Copyright <span className="text-purple-300">©</span> 2024 DcodeBlock</p>
           <div className="flex gap-4">
               <p className="text-medium">Help Center</p>
@@ -9,7 +9,6 @@ const Footer = () => {
               <p className="text-medium">Terms and Conditions</p>
               <div className=" border-l-2 border-white"></div>
               <p className="text-medium">Privacy policy</p>
-              
           </div>
     </div>
   )
--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='flex flex-col '>
+        <div className='flex flex-col bg-gradient-to-b from-black via-[#1d151f] via-[#1c151d] via-[#2e2324] to-[#201d20]'>
             <Header />
             <main className="">
                 {children}
